54245bb95efa19632f37f02fac5348d873f5db3a
rahulpatel1994 <rahuldpatel@ufl.edu>
2018-03-24 16:37:10 -0400

    Update background.js

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ var myApp = angular.module('SummarizerExtension', ['ngRoute']);
 myApp.controller("PopupListController", function ($scope) {
 
 var URL;
+var textapi;
 const APPLICATION_KEY = "1b955de3207750463ac95bd5481207ac";
 APPLICATION_ID = "f6a32887";
 
@@ -10,23 +11,27 @@ $( document ).ready(function() {
     chrome.tabs.sendMessage(tabs[0].id, {'command': 'getURL'}, function(response) {
       URL = response;
       var AYLIENTextAPI = require('aylien_textapi');
-      var textapi = new AYLIENTextAPI({
+      textapi = new AYLIENTextAPI({
           application_id: APPLICATION_ID,
           application_key: APPLICATION_KEY
       });
-      textapi.summarize({
-        url: URL,
-        sentences_number: 2
-      }, function(error, response) {
-        if (error === null) {
-          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {'command': 'highlight', 'text':response.sentences});
-            });
-            $scope.sentences = response.sentences;
-            $scope.$apply();
-            }
-          });
-        });
-      });
+      summarizeTex(textapi);
     });
+  });
+});
+
+function summarizeTex(textapi){
+  textapi.summarize({
+    url: URL,
+    sentences_number: 5
+    },function(error, response) {
+      if (error === null) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {'command': 'highlight', 'text':response.sentences});
+      });
+        $scope.sentences = response.sentences;
+        $scope.$apply();
+      }
+    });
+  }
 });
