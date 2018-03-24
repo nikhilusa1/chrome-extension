16b886e2fe543e5c33b30e1c7a052c3e2fbcc82c
rahulpatel1994 <rahuldpatel@ufl.edu>
2018-03-24 14:42:10 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,29 +1,32 @@
-myApp.controller("PopupListController", function ($scope) {
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
 var myApp = angular.module('SummarizerExtension', ['ngRoute']);
+myApp.controller("PopupListController", function ($scope) {
 
-var URL = response;
-const APPLICATION_KEY = "1b955de3207750463ac95bd5481207ac",
+var URL;
+const APPLICATION_KEY = "1b955de3207750463ac95bd5481207ac";
 APPLICATION_ID = "f6a32887";
 
-var AYLIENTextAPI = require('aylien_textapi');
-var textapi = new AYLIENTextAPI({
-    application_id: APPLICATION_ID,
-    application_key: APPLICATION_KEY
-});
-
-textapi.summarize({
-  url: URL,
-  sentences_number: 2
-}, function(error, response) {
-  if (error === null) {
-    alert(response.sentences);
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {'command': 'highlight', 'text':response.sentences});
+$( document ).ready(function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {'command': 'getURL'}, function(response) {
+      URL = response;
+      var AYLIENTextAPI = require('aylien_textapi');
+      var textapi = new AYLIENTextAPI({
+          application_id: APPLICATION_ID,
+          application_key: APPLICATION_KEY
       });
-      $scope.sentences = response.sentences;
-      $scope.$apply();
-      }
+      textapi.summarize({
+        url: URL,
+        sentences_number: 2
+      }, function(error, response) {
+        if (error === null) {
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {'command': 'highlight', 'text':response.sentences});
+            });
+            $scope.sentences = response.sentences;
+            $scope.$apply();
+            }
+          });
+        });
+      });
     });
-  });
 });
