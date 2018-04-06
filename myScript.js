064aad414a22a1be49571d570d91890b25f0b0ff
rahulpatel1994 <rahuldpatel@ufl.edu>
2018-04-06 00:08:36 -0400

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,9 +1,7 @@
-chrome.runtime.sendMessage(window.location.href);
-
-// chrome.runtime.onMessage.addListener(
-// 	function(request, sender, sendResponse) {
-// 		if(request.command == "getURL") {
-// 			var URL = window.location.href;
-// 		}
-// 		sendResponse(URL);
-// 	});
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if(request.command == "getURL") {
+			var URL = window.location.href;
+		}
+		sendResponse(URL);
+	});
