ac649bbf7959ec05ca9ffb04067d533607b4c4df
rahulpatel1994 <rahuldpatel@ufl.edu>
2018-04-06 01:45:49 -0400

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -8389,6 +8389,8 @@ myApp.controller("PopupListController", function ($scope) {
 
 var URL;
 var textapi;
+var font_size_index = 1;
+var font_sizes = ['small', 'medium', 'large', 'x-large', 'xx-large'];
 
 $( document ).ready(function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -8399,17 +8401,48 @@ $( document ).ready(function() {
           application_id: APPLICATION_ID,
           application_key: APPLICATION_KEY
       });
-      var num = 10;
+      var num = 3;
       summarizeText(num);
     });
   });
 });
 
+$(document).ready(function () {
+  $(".nav li").removeClass("active");//this will remove the active class from
+                                     //previously active menu item
+  $('#home').addClass('active');
+});
+
 $('#insert').keypress(function(e){
   if(e.which == 13){
-    var num_sentences = $(this).val();
-    summarizeText(num_sentences);
-    $(this).val('');
+    $('#search').click();
+  }
+});
+
+$('#search').click(function(){
+  var num_sentences = $('#insert').val();
+  summarizeText(num_sentences);
+  $('#insert').val('');
+});
+
+$('#schmalz_font').click(function(){
+  if(font_size_index != 4){
+    $('#summary_container').css('font-size', 'xx-large');
+    font_size_index = 4;
+  }
+});
+
+$('#int_font').click(function(){
+  if(font_size_index != 4){
+    $('#summary_container').css('font-size', font_sizes[font_size_index + 1]);
+    font_size_index++;
+  }
+});
+
+$('#dec_font').click(function(){
+  if(font_size_index != 0){
+    $('#summary_container').css('font-size', font_sizes[font_size_index - 1]);
+    font_size_index--;
   }
 });
 
@@ -8429,12 +8462,76 @@ function summarizeText(val){
   }
 });
 
+
 /****************************************************************
 HASHTAG STUFF DOWN BELOW!
 ****************************************************************/
 
 var myApp = angular.module('HashtagSuggestion', ['ngRoute']);
 myApp.controller("HashtagController", function ($scope) {
+
+var textapi;
+var URL;
+var tag_size = ['','','','','','','','','',''];
+var tag_size2 = ['','','','','','','','','',''];
+var flag = false;
+
+$( document ).ready(function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {'command': 'getURL'}, function(response) {
+      URL = response;
+      var AYLIENTextAPI = require('aylien_textapi');
+      textapi = new AYLIENTextAPI({
+        application_id: APPLICATION_ID,
+        application_key: APPLICATION_KEY
+      });
+      //Add function calls below
+      Hashtag(textapi);
+    });
+  });
+});
+
+$(document).ready(function () {
+  $(".nav li").removeClass("active");//this will remove the active class from
+                                     //previously active menu item
+  $('#tag').addClass('active');
+});
+
+$('#add_tag').click(function(){
+  flag = true;
+  Hashtag(textapi);
+});
+
+function Hashtag(textapi){
+  textapi.hashtags({
+    url: URL
+    },function(error, response) {
+      if (error === null) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {'text':response.hashtags});
+      });
+        for(var i = 0; i < tag_size.length; i++){
+          tag_size[i] = response.hashtags[i];
+        }
+        $scope.hashtags = tag_size;
+        if(flag == true){
+          for(var i = 0; i < tag_size2.length; i++){
+            tag_size2[i] = response.hashtags[i+10];
+          }
+          $scope.hashtags2 = tag_size2;
+        }
+        $scope.$apply();
+      }
+    });
+  }
+});
+
+/****************************************************************
+CITAION STUFF DOWN BELOW!
+****************************************************************/
+
+var myApp = angular.module('CitaionCreation', ['ngRoute']);
+myApp.controller('CitaionController', function ($scope) {
 
 var textapi;
 var URL;
@@ -8448,16 +8545,26 @@ $( document ).ready(function() {
         application_id: APPLICATION_ID,
         application_key: APPLICATION_KEY
       });
+      //Add function calls below
 
     });
   });
 });
 
 function Hashtag(textapi){
-
-}
-
-
+  textapi.summarize({
+    url: URL,
+    sentences_number: val
+    },function(error, response) {
+      if (error === null) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {'text':response.sentences});
+      });
+        $scope.sentences = response.sentences;
+        $scope.$apply();
+      }
+    });
+  }
 });
 
 },{"aylien_textapi":40}]},{},[41]);
